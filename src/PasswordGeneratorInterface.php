be703a41f8feef71fced257f3df2cee1c1363253
nika-gelashvili <nika.gelash@gmail.com>
2022-01-03 02:48:44 +0400

    Implement setters and getters for parameters and also generatePassword into PasswordGeneratorInterface

--- src/PasswordGeneratorInterface.php
+++ src/PasswordGeneratorInterface.php
@@ -4,5 +4,34 @@ namespace Fitpass\Generator;
 
 interface PasswordGeneratorInterface
 {
+    /**
+     * Set password generation option length
+     * @param $value
+     * @return mixed
+     */
+    public function setLength($value);
 
+    /**
+     * Set password generation option strength
+     * @param $value
+     * @return mixed
+     */
+    public function setStrength($value);
+
+    /**
+     * Get password generation option length
+     * @return mixed
+     */
+    public function getLength();
+
+    /**
+     * Get password generation option strength
+     * @return mixed
+     */
+    public function getStrength();
+
+    /**
+     * Generate password based on length and strength
+     */
+    public function generatePassword();
 }
